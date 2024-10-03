457c0353c9efb656e748bf82f2d0f268138545b2
Biranavan-Sritharan <biranavan07@gmail.com>
2024-10-04 00:11:26 +0100

    THIS PROJECT IS NOW FINISHED

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ The maze can also be changed really easily if needed to by changing numbers in t
 - 5 = trail/the path maze-solver has taken
 - 6 = wall collision, when the maze solver encounters a wall in its path its changes it to a 6 and then finds a different route
 
+### maze 1 and 2 txt files:
+Just some files with some mazes I used during testing, maze-solver 6 can complete both of these and other mazes I made :)
+
 ## maze-solver 1
 This maze solver moves only right and when it encounters a wall will move one space back and one row down then carry on moving right. End points that are behind walls can not be solved or end points on the left side will have a 50/50 chance at getting there depending on maze layout.
 
@@ -18,3 +21,16 @@ This maze-solver was more of a test run using try and except when reaching index
 
 ## maze-solver 3
 Can now handle dead ends in a maze, also looks for alternate routes when there is an intersection and stores these coordinates as a tuple.
+
+## maze-solver 4
+Added the beginning pieces of the backtracking and also able to detect vertical dead ends!
+
+## maze-solver 5
+Can now find the end point with its checks and doesn't just pass it, ALSO can now backtrack and search basically every square if it needs to
+
+## maze-solver 6
+Fixed an issue where if the start area had a vertical wall next to it, it would terminate itself prematurely. Fixed this by fixing the try and except.
+This maze solver should be able to solve 99% of mazes
+
+Also I commented out a lot of debug print statements, so if you want to see the alternate coordinates array being added to to get a better sense
+at how it works just ctrl + f and search for: print(alt_path). Theres also a bunch more print statements if you are intrested as well.
---
+++ maze-solver-6.py
@@ -0,0 +1,243 @@
+#2 is start point
+#3 is end point
+#1 is a wall
+#0 is empty space that can be walked in
+#5, with this we can see the path the maze solver has taken!
+#6 when it hits a wall it changes the 1 to a 6
+
+maze = [
+    [1,1,1,1,1,1,1,1,1],
+    [1,2,1,0,1,0,0,3,1],
+    [1,0,1,0,1,0,1,1,1],
+    [1,0,1,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1]
+]
+
+#gets the starting coords, in this case 2 is starting pos in maze
+start_row_count = 0
+start_col_count = 0
+found_start_row_count = 0
+found_start_col_count = 0
+for row in maze:
+    # print(x)
+    for col in row:
+        # print(y)
+        if start_col_count > len(row):
+            start_col_count = start_col_count - len(row)
+        if col == 2:
+           found_start_col_count = start_col_count
+           found_start_row_count = start_row_count
+        start_col_count += 1
+    start_row_count += 1
+
+print("starting row: " + str(found_start_row_count) + " starting col: " + str(found_start_col_count))
+print(maze[found_start_row_count][found_start_col_count]) #checks if obtained coords are correct, this should output 2 on terminal
+
+
+#variables used in below loop
+row_count = found_start_row_count 
+col_count = found_start_col_count
+path_trail = 5
+wall_check = 6
+end_loop = 0
+corner_check = 0
+win_break = 0
+
+dead_end_new_path = 0 #alts between 1 and 0
+alt_path = []
+
+for row in maze:
+    for col in row:
+
+        if dead_end_new_path > 0:
+            print("new pos")
+            row_count = alt_path_row_coord
+            col_count = alt_path_col_coord
+            pos = maze[row_count][col_count]
+            dead_end_new_path = 0
+            # print("before del")
+            # print("pos 2: " + str(pos))
+            # print(alt_path)
+            del alt_path[dead_end_new_path]
+            # print("after del")
+            # print("pos 1: " + str(pos))
+            # print(alt_path)
+            # print("")
+
+        else:
+            pos = maze[row_count][col_count]
+
+        if pos == 2:
+            print("start")
+            col_count += 1
+
+        if (pos == 0) or (pos == 5): #or pos == 5
+            maze[row_count][col_count] = path_trail #basically current pos but also trailed added to curretn pos
+            print("space")
+
+            possible_paths = 0
+
+            #left pos path check, for now just variables declared  ##update: now added in left pos coords
+            left_row_count = row_count
+            left_col_count = col_count - 1
+
+            if maze[left_row_count][left_col_count] == 0:
+                possible_paths += 1
+
+            #above pos path check
+            above_row_count = row_count - 1
+            above_col_count = col_count
+            if maze[above_row_count][above_col_count] == 0 or maze[above_row_count][above_col_count] == 0: #add 5 later mayb
+                possible_paths += 1
+
+            #below pos path check
+            below_row_count = row_count + 1
+            # print("below row: " + str(below_row_count))
+            # print("current row for below area: " + str(row_count))
+            below_col_count = col_count
+            
+            # print("below pos value: " + str(maze[below_row_count][below_col_count]))
+            if maze[below_row_count][below_col_count] == 0: #add 5 later mayb
+                possible_paths += 1
+
+            #right pos path check
+            right_row_count = row_count
+            right_col_count = col_count + 1
+            if maze[right_row_count][right_col_count] == 0 or maze[right_row_count][right_col_count] == 0: #add 5 later mayb   ###the 'solver' goes right by default anyways so no append to alt_path here
+                possible_paths += 1
+
+            #if end is in check, move pos to win area  ##this bascially stops the pos of maze solver just goin past the finish line and just moves it to the end to end the solving
+            if (maze[right_row_count][right_col_count] == 3):
+                row_count = right_row_count
+                col_count = right_col_count
+                win_break = 1
+                break
+            
+            if (maze[left_row_count][left_col_count] == 3):
+                row_count = left_row_count
+                col_count = left_col_count
+                win_break = 1
+                break
+
+            if (maze[above_row_count][above_col_count] == 3):
+                row_count = above_row_count
+                col_count = above_col_count
+                win_break = 1
+                break
+            
+            if (maze[below_row_count][below_col_count] == 3):
+                row_count = below_row_count
+                col_count = below_col_count
+                win_break = 1
+                break
+
+            if possible_paths >= 2 and maze[above_row_count][above_col_count] == 0 or maze[above_row_count][above_col_count] == 0:
+                alt_path.append((above_row_count,above_col_count))
+            
+            if possible_paths >= 2 and maze[below_row_count][below_col_count] == 0:
+                alt_path.append((below_row_count,below_col_count))
+
+            if possible_paths >= 2 and maze[left_row_count][left_col_count] == 0:
+                alt_path.append((left_row_count,left_col_count))
+
+            ### situations/scenarios event handling ###
+            #dead end check (horizontal dead end)
+            if maze[right_row_count][right_col_count] == 1 and maze[above_row_count][above_col_count] == 1 and maze[below_row_count][below_col_count] == 1:
+                print("dead end")
+                coords = alt_path[0]
+                alt_path_row_coord = coords[0]
+                alt_path_col_coord = coords[1]
+
+                dead_end_new_path = 1
+                if dead_end_new_path > len(alt_path):
+                    dead_end_new_path = 0
+                #also once pos moved to new coords REMOVE it from alt_path
+                #move pos to one of the other coords from alt_path array
+
+            #vertical dead end check
+            elif (maze[above_row_count][above_col_count] == 1 or maze[above_row_count][above_col_count] == 6) and (maze[right_row_count][right_col_count] == 1 or maze[right_row_count][right_col_count] == 6) and (maze[left_row_count][left_col_count] == 1 or maze[left_row_count][left_col_count] == 6):
+                print("vertical dead end")
+                coords = alt_path[0]
+                alt_path_row_coord = coords[0]
+                alt_path_col_coord = coords[1]
+
+                dead_end_new_path = 1
+                if dead_end_new_path > len(alt_path):
+                    dead_end_new_path = 0
+
+            #trying to use up rest of the alt routes here
+            elif maze[right_row_count][right_col_count] == 5 or pos == 5:
+                # print("trail overlap")
+                # print("current row: " +str(row_count) + " current col: " + str(col_count))
+                # print("right row: " +str(right_row_count) + " right col: " + str(right_col_count))
+                # print("current pos: " + str(pos))  
+                try:
+                    coords = alt_path[0]  #now when game starts this is more or less bascially empty, so yh ://///////
+                except:
+                    #alt_path.append((row_count + 1,col_count))  #ok, so this solution is kinda fragile - can cause clipping thru walls
+                    #print("below pos in except: " + str(maze[below_row_count][below_col_count])) #this solution is pretty unstable...probably :)
+                    if maze[below_row_count][below_col_count] == 0:
+                        alt_path.append((below_row_count,below_col_count))
+                        coords = alt_path[0]
+                    elif maze[below_row_count][below_col_count] == 5:
+                        alt_path.append((below_row_count,below_col_count))
+                        coords = alt_path[0]
+                    elif maze[below_row_count][below_col_count] == 1:
+                        alt_path.append((below_row_count - 1,below_col_count - 1))
+                        coords = alt_path[0]
+
+                # finally:
+                #     print("end not found")
+                #     for x in maze:
+                #         print(x)
+                #print(alt_path)
+                alt_path_row_coord = coords[0]
+                alt_path_col_coord = coords[1]
+
+                dead_end_new_path = 1
+                # if dead_end_new_path > len(alt_path):
+                #     dead_end_new_path = 0
+
+            #corner check, bottom right corner
+            elif maze[right_row_count][right_col_count] == 1 and maze[below_row_count][below_col_count] == 1:
+                print("bottom right corner")
+                if maze[above_row_count][above_col_count] == 0:
+                    row_count -= 1
+
+            #vertical column from bottom right corner scenario
+            elif (maze[right_row_count][right_col_count] == 1 or maze[right_row_count][right_col_count] == 6) and (maze[left_row_count][left_col_count] == 1 or maze[left_row_count][left_col_count] == 6):
+                row_count -= 1
+
+            else:
+                col_count += 1
+
+            if win_break == 1:
+                break
+
+        if pos == 1 or pos == 6:
+            print("wall")
+            corner_check += 1
+            maze[row_count][col_count] = wall_check
+            col_count -= 1
+            row_count += 1
+
+        if pos == 3:
+            print("Reached End Point!")
+            end_loop = 1
+            break
+
+    if end_loop == 1:
+        end_loop = 0
+        break
+
+#print(alt_path)
+for x in maze:
+    print(x)
+
+#some updating array check
+# print(maze[2][2])
+# new = 5
+# maze[2][2] = 5
+# print(maze[2][2])
